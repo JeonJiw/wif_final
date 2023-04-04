cad9ff78ea65fcc89cf62a6feb1b7ec8ba888936
Jiwon Jeon <87048311+Jiwonemil@users.noreply.github.com>
2023-04-03 22:19:00 -0600

    add openNav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-var currentDate = new Date();
-var currentYear = currentDate.getFullYear();
-
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
 document.getElementById("current-year").innerHTML = currentYear;
+
+let navbar = document.getElementById("nav-bar");
+let menuIcon = document.getElementById("menu-icon");
+
+let open = 0;
+function openNav() {
+  if (open === 0) {
+    navbar.style.display = "block";
+    open = 1;
+  } else {
+    navbar.style.display = "none";
+    open = 0;
+  }
+}
